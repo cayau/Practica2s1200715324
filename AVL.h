ef3c2e12179c5c3b199e0d925e0d5f1d3a9c47fc
cayau <ayau15@gmail.com>
2015-04-06 01:47:37 -0600

    Insercion y Recorrido AVL
    Insercion de datos desde archivo .txt a el arbol AVL.
    Recorrido en orden en arbol AVL.
    Toma de tiempos en insercion y recorrido.

---
+++ AVL.h
@@ -0,0 +1,148 @@
+#ifndef AVL_H_INCLUDED
+#define AVL_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *left,*right;
+    int ht;
+}node;
+
+node *insert(node *,int);
+void  inorder(node *);
+int   height( node *);
+node *rotateright(node *);
+node *rotateleft(node *);
+node *RR(node *);
+node *LL(node *);
+node *LR(node *);
+node *RL(node *);
+int FE(node *);
+
+node * insert(node *T,int x)
+{
+    if(T==NULL)
+    {
+        T=(node*)malloc(sizeof(node));
+        T->data=x;
+        T->left=NULL;
+        T->right=NULL;
+    }
+    else
+        if(x > T->data)                // insert in right subtree
+        {
+            T->right=insert(T->right,x);
+            if(FE(T)==-2){
+                if(x>T->right->data){
+                    T=RR(T);
+                }else{
+                    T=RL(T);
+                }
+            }
+        }
+        else
+            if(x<T->data)
+            {
+                T->left=insert(T->left,x);
+                if(FE(T)==2){
+                    if(x < T->left->data){
+                        T=LL(T);
+                    }else{
+                        T=LR(T);
+                    }
+                }
+            }
+        T->ht=height(T);
+    return(T);
+}
+
+int height(node *T)
+{// Height off three
+   int lh,rh;
+   if(T==NULL)
+      return(0);
+   if(T->left==NULL)
+      lh=0;
+   else
+      lh=1+T->left->ht;
+   if(T->right==NULL)
+      rh=0;
+   else
+      rh=1+T->right->ht;
+   if(lh>rh)
+      return(lh);
+   return(rh);
+}
+node * rotateright(node *x)
+{//Rotate Right
+   node *y;
+   y=x->left;
+   x->left=y->right;
+   y->right=x;
+   x->ht=height(x);
+   y->ht=height(y);
+   return(y);
+}
+node * rotateleft(node *x)
+{//Rotate Left
+   node *y;
+   y=x->right;
+   x->right=y->left;
+   y->left=x;
+   x->ht=height(x);
+   y->ht=height(y);
+   return(y);
+}
+node * RR(node *T)
+{
+    T=rotateleft(T);
+    return(T);
+}
+node * LL(node *T)
+{
+   T=rotateright(T);
+   return(T);
+}
+node * LR(node *T)
+{
+   T->left=rotateleft(T->left);
+   T=rotateright(T);
+   return(T);
+}
+node * RL(node *T)
+{
+   T->right=rotateright(T->right);
+   T=rotateleft(T);
+   return(T);
+}
+int FE(node *T)
+{
+   int lh,rh;
+   if(T==NULL)
+   return(0);
+   if(T->left==NULL)
+      lh=0;
+   else
+      lh=1+T->left->ht;
+   if(T->right==NULL)
+      rh=0;
+   else
+      rh=1+T->right->ht;
+   return(lh-rh);
+}
+
+void inorder(node *T)
+{
+   if(T!=NULL)
+   {
+      inorder(T->left);
+      printf("*");
+      //printf("%d(FE=%d) ",T->data,FE(T));
+      inorder(T->right);
+   }
+}
+
+#endif // AVL_H_INCLUDED
--- main.c
+++ main.c
@@ -1,8 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "AVL.h"
+
+void ingresarArbol();
+void ingresarLista();
+
+clock_t ini1, ini2, ini3, ini4;
+clock_t fin1, fin2, fin3, fin4;
+float t1, t2, t3, t4;
+FILE *file;
+char *path;
+node *raiz = NULL;
 
 int main()
 {
-    printf("Hello world!\n");
+    int op;
+
+    do{
+        system("cls");
+        printf("********************* PRACTICA 2 ********************\n");
+        printf(" \nSeleccione una Opcion\n");
+        printf(" 1) Ingresar arbol\n");
+        printf(" 2) Recorrer arbol\n");
+        printf(" 3) Ordenamiento Burbuja\n");
+        printf(" 4) Ordenamiento Quicksort\n");
+        printf(" 5) Salir\n");
+        printf(" Ingrese el numero de la opcion deceada:");
+        scanf("%d",&op);
+        switch(op)
+        {
+            case 1:
+                printf("Ingrese la direccion del archivo con los datos: \n");
+                path = (char *)malloc(128 *sizeof(char));
+                scanf("%s", path);
+                ini1 = clock();
+                ingresarArbol();
+                fin1 = clock();
+                t1 = (float)((fin1-ini1)/CLOCKS_PER_SEC);
+                printf("\n Ingresar arbol - %f segundos\n",t1);
+                system("pause");
+                break;
+            case 2:
+                ini2 = clock();
+                if(raiz==NULL){
+                    printf(" ARBOL VACIO!!");
+                }else{
+                    inorder(raiz);
+                }
+                fin2 = clock();
+                t2 = (float)((fin2-ini2)/CLOCKS_PER_SEC);
+                printf("\n Recorrido arbol - %f segundos\n",t2);
+                system("pause");
+                break;
+            case 3:
+                system("pause");
+                break;
+            case 4:
+                system("pause");
+                break;
+        }// End switch
+    }while(op!=5);
     return 0;
+}
+
+void ingresarArbol(){
+    file = fopen(path,"r");
+    if (file==NULL){
+        printf(" Archivo no encontrado o no existe");
+    }else{
+        char buf[15];
+        while (fgets(buf, sizeof buf, file) != NULL){
+            int val = atol(buf);
+            raiz = insert(raiz, val);
+            printf(".");
+        }
+        fclose(file);
+    }
 }
